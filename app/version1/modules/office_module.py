7e175d0b5d9219d3fbe04f7d05bcb58e38d3849c
evanswanjau <evanswanjau@gmail.com>
2019-02-07 14:49:49 +0300

    Added Modules for Creating a Government Office
    - Checked whether that data already exists
    - Redirected respective agecases
    - Return expected value and format
    
    [feature #163682980]

---
+++ app/version1/modules/office_module.py
@@ -0,0 +1,48 @@
+""" A class to handle government office methods """
+from flask import abort
+from .party_module import PoliticalParty
+
+class GovernmentOffice(PoliticalParty):
+
+    """ Methods and Attributes for GovernmentOffice. """
+
+    def __init__(self, id=None, type=None, name=None, office_data=None):
+        self.id = id
+        self.type = type
+        self.name = name
+        self.office_data = office_data
+
+
+    def create_office(self):
+
+        # id
+        if not self.id:
+            abort(400)
+        else:
+            if GovernmentOffice.check_existence(self, "id", self.id, self.office_data):
+                abort(409)
+            else:
+                id = self.id
+
+        # type
+        if not self.type:
+            abort(400)
+        else:
+            type = self.type
+
+
+        # name
+        if not self.name:
+            abort(400)
+        else:
+            if GovernmentOffice.check_existence(self, "name", self.name, self.office_data):
+                abort(409)
+            else:
+                name = self.name
+
+
+        return {
+            "id": id,
+            "type": type,
+            "name": name,
+        }
