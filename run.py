c0a6ea4e244f362eeb45f6d10bd8da4b9404379d
evanswanjau <evanswanjau@gmail.com>
2019-02-08 23:51:18 +0300

    Made changes to run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
-from app import create_app
+from app.__init__ import create_app
 
-app = create_app("development")
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
