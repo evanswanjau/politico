7ac2d4e844ec08d5cbacb9e0afd9342c1ef02b11
evanswanjau <evanswanjau@gmail.com>
2019-02-09 00:25:02 +0300

    Made changes to run.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
-
     from .version1.views import admin
     app.register_blueprint(admin.bp)
 
@@ -29,25 +28,6 @@ def create_app(test_config=None):
         pass
 
 
-    # error handdling
-    @app.errorhandler(400)
-    def bad_request(error):
-        return make_response(jsonify({'status': 400, 'error': 'Invalid Request'}), 400)
 
-    @app.errorhandler(403)
-    def forbidden_request(error):
-        return make_response(jsonify({'status': 403, 'error': 'Account Inaccessible'}), 403)
-
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return make_response(jsonify({'status': 404, 'error': 'Not Found'}), 404)
-
-    @app.errorhandler(405)
-    def not_allowed(error):
-        return make_response(jsonify({'status': 405, 'error': 'Not Allowed'}), 405)
-
-    @app.errorhandler(409)
-    def already_exists(error):
-        return make_response(jsonify({'status': 409, 'error': 'Already Exists'}), 409)
 
     return app
