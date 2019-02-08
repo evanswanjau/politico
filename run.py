58a15097763ed190f5ee720f0c2055f0833f16a6
evanswanjau <evanswanjau@gmail.com>
2019-02-08 22:16:36 +0300

    Made changes to run.py

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
 import os
-
 from app import create_app
 
-app = create_app()
-
-# a simple page that says hello
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+app = create_app("development")
 
 if __name__ == '__main__':
     app.run(debug=True)
