d137a10632f83d8be69c9e7709dd01e71c28d2e7
evanswanjau <evanswanjau@gmail.com>
2019-02-14 02:12:47 +0300

    Refactored Code Into Class Based APIs
    - Created classes to habdle the APIs.
    - Implemented the methods on the APIs
    - Refactors the new modules to fit the modules
    
    [finishes #163898018]

--- app/version1/views/admin.py
+++ app/version1/views/admin.py
@@ -1,168 +1,40 @@
-""" This is all the functions the admin will take care of """
-from flask import Flask, Blueprint, request, jsonify, make_response
-from ..modules.party_module import PoliticalParty
-from ..modules.office_module import GovernmentOffice
+""" This is all the api the admin is in charge of """
 
-BP = Blueprint('admin', __name__, url_prefix='/api/v1/')
+from flask import Blueprint
+from ..views.api_modules.api_module import *
 
-party_list = [
-    {
-        "party_id": 1,
-        "party_name": "blue party",
-        "chairman": "hammer delta",
-        "hqaddress":"474 Major Road",
-        "logoUrl":"blueparty.img"
-    },
-    {
-        "party_id": 2,
-        "party_name": "red party",
-        "chairman": "glazer chimmy",
-        "hqaddress":"90 Fifth Street",
-        "logoUrl":"redparty.img"
-    }
-]
+admin_bp = Blueprint('admin', __name__, url_prefix='/api/v1/')
 
-
-office_list = [
-    {
-        "id": 1,
-        "type": "state",
-        "name": "president"
-    },
-    {
-        "id": 2,
-        "type": "state",
-        "name": "vice president"
-    }
-]
-
-
-# create political party endpoint
-@BP.route('create-political-party', methods=['POST'])
-def create_political_party():
-    """ Create political party emdpoint """
-    data = request.get_json()
-
-    political_party = PoliticalParty(data['party_id'], data['party_name'],
-                                     data['chairman'], data['hqaddress'],
-                                     data['logoUrl'], party_list)
-
-    new_party = political_party.create_party()
-
-    party_list.append(new_party)
-
-    return make_response(jsonify({
-        "status": 200,
-        "data": [{"id":new_party.get("party_id"), "name":new_party.get("party_name")}]
-    }), 200)
-
+party_view = PartyAPI.as_view('political_party_api')
 
 # view all political parties
-@BP.route('political-parties', methods=['GET'])
-def political_parties():
-    """ This will get all political parties """
-    political_parties = PoliticalParty(party_data=party_list)
+admin_bp.add_url_rule('/parties/', defaults={'party_id': None},
+                      view_func=party_view, methods=['GET'])
 
-    political_parties = political_parties.get_all_political_parties()
+# view single political party
+admin_bp.add_url_rule('/parties/<int:party_id>', view_func=party_view, methods=['GET'])
 
-    return make_response(jsonify({
-        "status": 200,
-        "data": political_parties
-    }), 200)
+# post a new political party
+admin_bp.add_url_rule('/parties', view_func=party_view, methods=['POST'])
 
+# edit a political party
+admin_bp.add_url_rule('/parties/<int:party_id>/name', view_func=party_view, methods=['PATCH'])
 
-# get a specific political party
-@BP.route('political-party/<int:party_id>', methods=['GET'])
-def get_political_party(party_id):
-    """ Get a specific political party """
+# delete a political party
+admin_bp.add_url_rule('/parties/<int:party_id>/', view_func=party_view, methods=['DELETE'])
 
-    single_political_party = PoliticalParty(party_data=party_list)
+"""
+Political Offices
+"""
 
-    party_info = single_political_party.get_specific_political_party(party_id)
+office_view = OfficeAPI.as_view('political_office_api')
 
-    return make_response(jsonify({
-        "status":200,
-        "data": party_info
-    }), 200)
+# post a new political office
+admin_bp.add_url_rule('/offices', view_func=office_view, methods=['POST'])
 
+# view all political offices
+admin_bp.add_url_rule('/offices', defaults={'office_id': None},
+                      view_func=office_view, methods=['GET'])
 
-# edit a specific political party
-@BP.route('edit-political-party/<int:party_id>', methods=['PATCH'])
-def edit_political_party(party_id):
-    """ This will enable the update of a political party """
-    data = request.get_json()
-
-    edit_political_party = PoliticalParty(party_id, data['party_name'],
-                                          data['chairman'], data['hqaddress'],
-                                          data['logoUrl'], party_list)
-
-    updated_party = edit_political_party.edit_political_party()
-
-    return make_response(jsonify({
-        "status": 201,
-        "data": updated_party
-    }), 201)
-
-
-# delete a specific political party
-@BP.route('delete-political-party/<int:party_id>', methods=['DELETE'])
-def delete_political_party(party_id):
-    """ This will enable the deletion of a political party """
-    delete_political_party = PoliticalParty(party_id=party_id, party_data=party_list)
-
-    message = delete_political_party.delete_political_party()
-
-    return make_response(jsonify({
-        "status": 201,
-        "message": message,
-    }), 201)
-
-
-
-#------------------------------------------------------
-# Government Office API's
-#------------------------------------------------------
-
-# create a government Office API
-@BP.route('create-gov-office', methods=['POST'])
-def create_government_office():
-
-    """ Create government office endpoint """
-
-    data = request.get_json()
-
-    office = GovernmentOffice(data['id'], data['type'], data['name'], office_list)
-
-    new_office = office.create_office()
-
-    office_list.append(new_office)
-
-    return make_response(jsonify({
-        "status": 200,
-        "data": [new_office]
-    }), 200)
-
-
-# view all government offices
-@BP.route('government-offices', methods=['GET'])
-def government_offices():
-    """ This will get all government offices """
-    return make_response(jsonify({
-        "status": 200,
-        "data": office_list
-    }), 200)
-
-
-# get a specific government office
-@BP.route('government-office/<int:office_id>', methods=['GET'])
-def get_gov_office(office_id):
-    """ This method will get a specific office """
-
-    specific_office = GovernmentOffice(office_data=office_list)
-
-    office_data = specific_office.get_specific_gov_office(office_id)
-
-    return make_response(jsonify({
-        "status":200,
-        "data": office_data
-    }), 200)
+# view single political office
+admin_bp.add_url_rule('/offices/<int:office_id>', view_func=office_view, methods=['GET'])
