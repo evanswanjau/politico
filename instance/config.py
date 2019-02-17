a8a0755d6e85f05f3437e527d9d7d59093ac09fc
evanswanjau <evanswanjau@gmail.com>
2019-02-18 02:51:30 +0300

    Set Up DB Connection
    - Set up DB connection.
    - Updated Config by adding db URL.
    - Created tables
    
    [chore #163897682]

--- instance/config.py
+++ instance/config.py
@@ -4,23 +4,27 @@ class Config(object):
     """ Our parent configuration class. """
     DEBUG = False
     SECRET = os.getenv('This is our lil secret')
+    Database_Url = os.getenv('politico_db')
 
 
 class DevelopmentConfig(Config):
     """ Configurations for Development. """
     DEBUG = True
+    Database_Url = os.getenv('politico_db')
 
 
 class TestingConfig(Config):
     """ Configurations for Testing. """
     TESTING = True
     DEBUG = True
+    #Database_Url = os.getenv('politico_test_db')
 
-
+# on heroku deployment
 class ProductionConfig(Config):
     """ Configurations for Production. """
     DEBUG = False
     TESTING = False
+    Database_Url = os.getenv('DATABASE_URL')
 
 
 app_config = {
