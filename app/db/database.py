9330bd1aea9d2df41dde0a61d425154ff58da79a
evanswanjau <evanswanjau@gmail.com>
2019-02-19 04:06:33 +0300

    Refactored DB
    - Build a base DB unit that runs on launch.
    - Build DB module that handles all db queries.
    - Refactored __init__ file.
    
    [starts #163897685]

---
+++ app/db/database.py
@@ -0,0 +1,71 @@
+"""" Connection to the postgres database """
+import psycopg2
+from .main_db import drop_existing_tables, create_tables, create_admin
+
+class DBConnection:
+    """ Connection for our entire application """
+
+    # initialize connection
+    def __init__(self, DB_URL):
+        """ initilize db connection with db url """
+        try:
+            self.connection = psycopg2.connect(DB_URL)
+            self.cursor  = self.connection.cursor()
+        except Exception as error:
+           print(error)
+
+
+    # create tables and admin
+    def create_tables_and_admin(self):
+        """ creates all tables """
+        tables = create_tables()
+
+        for table in tables:
+            self.cursor.execute(table)
+            self.connection.commit()
+
+        create_admin(self.connection)
+
+
+    # drop all tables
+    def drop_tables(self):
+        """ Deletes all tables """
+        tables = drop_existing_tables()
+
+        for table in tables:
+            self.cursor.execute(table)
+            self.connection.commit()
+
+
+    # fetch a single item
+    def fetch_single_item(self, query):
+        """ fetches a single item """
+        self.cursor.execute(query)
+        item = self.cursor.fetchone()
+        return item
+
+
+    # fetch multiple items
+    def fetch_multiple_items(self, query):
+        """ fetches multiple items """
+        self.cursor.execute(query)
+        items = cur.fetchall()
+        return items
+
+
+    # insert data
+    def insert_data(self, table, data):
+        """ insert data """
+        keys = tuple(', '.join([key for key in data]))
+        data_values = tuple([data[key] for key in data])
+
+        self.cursor.execute(""" INSERT INTO table keys VALUES data_values """)
+        self.connection.commit()
+
+
+    # update data
+    def update_data(self, table, key, value, field, id):
+        """ update data """
+        query = """ UPDATE table SET key = value WHERE field = id """
+        self.cursor.execute(query)
+        self.connection.commit()
---
+++ app/db/main_db.py
@@ -0,0 +1,84 @@
+""" Modules that will run on app initialization """
+
+# method to drop existing tables
+def drop_existing_tables():
+    """ Drops all tables """
+    tables_to_drop = [
+        """ DROP TABLE IF EXISTS party """,
+        """ DROP TABLE IF EXISTS users """,
+        """ DROP TABLE IF EXISTS office """,
+        """ DROP TABLE IF EXISTS candidates """,
+        """ DROP TABLE IF EXISTS vote """,
+        """ DROP TABLE IF EXISTS petition """
+    ]
+
+    return tables_to_drop
+
+# method to create politico tables
+def create_tables():
+    """ create tables for our app """
+    tables_to_create = [
+        """
+        CREATE TABLE party (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            hqAddress VARCHAR(50) NOT NULL,
+            logoUrl VARCHAR(50) NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            firstname VARCHAR(50) NOT NULL,
+            secondname VARCHAR(50) NOT NULL,
+            othername VARCHAR(50),
+            email VARCHAR(50) NOT NULL,
+            phoneNumber VARCHAR (15) NOT NULL,
+            passportUrl VARCHAR (50) NOT NULL,
+            isAdmin BOOLEAN NOT NULL DEFAULT FALSE
+        )
+        """,
+        """
+        CREATE TABLE office (
+            id SERIAL PRIMARY KEY,
+            type VARCHAR(50) NOT NULL,
+            name VARCHAR(50) NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE candidates (
+            id SERIAL PRIMARY KEY,
+            office INTEGER NOT NULL,
+            party INTEGER NOT NULL,
+            candidate INTEGER NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE vote (
+            id SERIAL PRIMARY KEY,
+            createdOn timestamp DEFAULT CURRENT_TIMESTAMP,
+            createdBy INTEGER NOT NULL,
+            office INTEGER NOT NULL,
+            candidate INTEGER NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE petition (
+            id SERIAL PRIMARY KEY,
+            createdOn timestamp DEFAULT CURRENT_TIMESTAMP,
+            createdBy INTEGER NOT NULL,
+            office INTEGER NOT NULL,
+            body TEXT,
+            evidence VARCHAR(100)
+        )
+        """]
+
+    return tables_to_create
+
+
+# method to create user admin
+def create_admin():
+    """ create admin user """
+    # admin object
+    # check whether admin exists
+    # insert admin if not exists
